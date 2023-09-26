7b38fda9b4f547950747c41736187e69ee695877
nineneel <sujarniel.setiadi@tiketkerja.com>
2023-09-26 20:23:12 +0800

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f97316",
+        "body-color": "#637381",
+        background: "#fff7ed",
+      },
+    },
+  },
+  plugins: [],
+};
